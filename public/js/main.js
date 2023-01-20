17fe74e6e9f74c410c68eb3762314f24ed4d301d
boonaki <joshdurr17@gmail.com>
2023-01-19 22:53:30 -0500

    minor changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,11 @@
 // OPENS/CLOSES MENU
 function menu(){
-    console.log('hello')
     let menu = document.getElementById('side-bar')
-    //if menu is not showing
+    //if menu is showing
     if(menu.classList.contains('translate-x-0')){
         menu.classList.remove('translate-x-0')
         menu.classList.add('translate-x-[100%]')
-    }else{ //if menus is showing
+    }else{ //if menus is not showing
         menu.classList.remove('translate-x-[100%]')
         menu.classList.add('translate-x-0')
     }
