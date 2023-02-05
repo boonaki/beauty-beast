acc88b1927607755fc67c04d95b4773bc70d02ca
Luis-Betancourt93 <betancourtluis619@gmail.com>
2023-02-05 14:12:29 -0800

    Added admin router

--- routes/main.js
+++ routes/main.js
@@ -7,11 +7,11 @@ const { ensureAuth, ensureGuest } = require('../middleware/auth');
 
 //Main Routes - simplified for now
 router.get("/", homeController.getIndex);
-router.get("/profile", ensureAuth, postsController.getProfile);
-router.get("/checkout", ensureAuth, postsController.getCheckout);
-router.get("/feed", ensureAuth, postsController.getFeed);
+router.get("/checkout", ensureAuth, productsController.getCheckout);
+router.get("/feed", ensureAuth, productsController.getFeed);
 router.get("/login", authController.getLogin);
 router.post("/login", authController.postLogin);
+router.get('/admin', ensureAuth, productsController.getProfile);
 router.get("/logout", authController.logout);
 router.get("/signup", authController.getSignup);
 router.post("/signup", authController.postSignup);
