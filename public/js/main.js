1be439e9c36b53c1749431a7ed290ab4a032a8ca
boonaki <joshdurr17@gmail.com>
2023-02-13 13:15:35 -0500

    changed header

--- public/js/main.js
+++ public/js/main.js
@@ -4,9 +4,9 @@ function menu(){
     //if menu is showing
     if(menu.classList.contains('translate-x-0')){
         menu.classList.remove('translate-x-0')
-        menu.classList.add('translate-x-[100%]')
+        menu.classList.add('translate-x-[-100%]')
     }else{ //if menus is not showing
-        menu.classList.remove('translate-x-[100%]')
+        menu.classList.remove('translate-x-[-100%]')
         menu.classList.add('translate-x-0')
     }
 }
